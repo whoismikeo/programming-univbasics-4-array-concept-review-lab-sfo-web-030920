eeabb4622f2204b0582b63e9233dcf3f141be459
Michael Ochnicki <ochnicki.michael@gmail.com>
2020-02-16 17:13:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,13 @@
 def find_element_index(array, value_to_find)
-  array.find_index(value_to_find)
+  return array.find_index(value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.sort
+  return array[array.length - 1]
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.sort
+  return array[0]
 end
